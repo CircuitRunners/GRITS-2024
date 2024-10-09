cbe824838d2b6552d120f9da77ca1d9a3120aee2
FryGuy <147878878+pranav1283@users.noreply.github.com>
2024-10-09 09:43:22 -0400

    Finished the Constants Class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,6 +16,7 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -206,5 +207,104 @@ public final class Constants{
     public static final double kD = 0;
     public static final int tofId = 0;
     public static final double tofThresholdMMS = Units.inchesToMeters(5) * 1000;
+  }
+
+  public static final class ShooterConstants{
+    public static final class FlywheelConstants{
+      public static final int shooterLeft = 10;
+      public static final int shooterRight = 11;
+      public static final double shooterOutSpeedRPM = 1500;
+      public static final double kS = 0.3;
+      public static final double kV = 1.5;
+      public static final double kA = 0.2;
+    }
+  
+    public static final class ArmConstants{
+      public static final double maxRadians = Units.degreesToRadians(45);
+      public static final double minRadians = Units.degreesToRadians(-65);
+      public static final double encoderThreshold = Units.degreesToRadians(0.);
+      public static final Rotation2d intakeRotation = Rotation2d.fromDegrees(-35);
+      public static final Rotation2d shootRotation = Rotation2d.fromRadians(maxRadians);
+      public static final Rotation2d ampRotation = Rotation2d.fromDegrees(-60);
+      public static final double kP = 3.0;
+      public static final double kI = 0;
+      public static final double kD = 0.2;
+      public static final double kS = 0.5;
+      public static final double kV = 0.5;
+      public static final double kG = 0.7;
+      public static final int armLeaderId = 9;
+      public static final int armFollowerId = 8;
+      public static final int throuhBoreEncoderPort = 9;
+      // Degrees Per Second
+      public static final double armSpeed = 10;
+    }
+
+    public static final class RollerConstants{
+      public static final double rollerInSpeed = 0.8;
+      public static final double rollerOutSpeed = -1;
+      public static final double rollerOutSpeedSlow = -0.25;
+
+      public static final int rollerID = 9;
+      public static final int tofId = 0;
+      public static final double tofThreshold = Units.inchesToMeters(17) * 1000;
+    }
+  }
+
+  public class FieldConstants{
+    public static final Measure<Distance> kFieldLength = Meters.of(16.54);
+    public static final Measure<Distance> kFieldWidth = Meters.of(8.21);
+
+    public static final AprilTagFieldLayout kFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+
+  // taken from 6328. All in blue alliance origin.
+  /* speaker constants */
+  public static final class SpeakerK {
+    private static final Measure<Distance> kTopX = Inches.of(18.055);
+    private static final Measure<Distance> kTopZ = Inches.of(83.091);
+    public static final Translation3d kTopRight = new Translation3d(kTopX, Inches.of(238.815), kTopZ);
+    public static final Translation3d kTopLeft = new Translation3d(kTopX, Inches.of(197.765), kTopZ);
+
+    private static final Measure<Distance> kBotX = Inches.of(0);
+    private static final Measure<Distance> kBotZ = Inches.of(78.324);
+    // private static final Translation3d kBotRight = new Translation3d(
+    // kBotX, Inches.of(238.815), kBotZ);
+    public static final Translation3d kBotLeft = new Translation3d(kBotX, Inches.of(197.765), kBotZ)
+    public static final Translation3d kBlueCenterOpening = kBotLeft.interpolate(kTopRight, 0.5);
+    public static final Pose3d kBlueCenterOpeningPose3d = new Pose3d(kBlueCenterOpening, new Rotation3d());
+    public static final Translation3d kRedCenterOpening = AllianceFlipUtil.flip(kBlueCenterOpening);
+    public static final Pose3d kRedCenterOpeningPose3d = new Pose3d(kRedCenterOpening, new Rotation3d());
+  }
+
+   /* amp constants */
+   public static final Measure<Distance> kXToAmp = Inches.of(49.5);
+   public static final Measure<Distance> kYToAmp = Inches.of(286.765);
+   public static final Measure<Distance> kZToAmp = Inches.of(35);
+
+   public static final Translation3d kBlueAmpPose = new Translation3d(
+       kXToAmp, kYToAmp, kZToAmp);
+   public static final Pose2d kBlueAmpPose2d = new Pose2d(
+       kXToAmp, kYToAmp, Rotation2d.fromDegrees(90));
+       
+   public static final Translation3d kRedAmpPose = new Translation3d(
+         kFieldLength.minus(kXToAmp), kYToAmp, kZToAmp);
+   public static final Pose2d kRedAmpPose2d = new Pose2d(
+       kRedAmpPose.getX(), kRedAmpPose.getY(), Rotation2d.fromDegrees(90));
+
+   /* stage constants */
+   public static final double kBlueStageClearanceDs = Units.inchesToMeters(188.5);
+   public static final double kBlueStageClearanceRight = Units.inchesToMeters(88.3);
+   public static final double kBlueStageClearanceCenter = Units.inchesToMeters(243.2);
+   public static final double kBlueStageClearanceLeft = Units.inchesToMeters(234.9);
+
+   public static final double kRedStageClearanceDs = Units.inchesToMeters(542.2);
+   public static final double kRedStageClearanceRight = Units.inchesToMeters(88.3);
+   public static final double kRedStageClearanceCenter = Units.inchesToMeters(407.9);
+   public static final double kRedStageClearanceLeft = Units.inchesToMeters(234.9);
+}
+
+  public static final class Vision{
+    public static final AprilTagFieldLayout fieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+    public static final Transform3d frontLeftCamTransform = new Transform3d();
+    public static final Transform3d frontRightCamTransform = new Transform3d();
   }
 }
