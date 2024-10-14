d0c70afb5d3b3d6010f5807a3f2a5911f4ab70e5
Kartikeya Shukla <kartikeya.shukla@circuitrunners.com>
2024-10-13 23:25:24 -0400

    I made a method called “Arm” that creates a new arm with an access specifier of public. In the method, I have the variable “arm” call a method called “configFactoryDefault()” and “.setNeutralMode()”. When I call the “.setNeutralMode()”, its a parameter setting it to brake.
    I also made a void method called resetArmEncoder, which calls the “.setSelectedSensorPosition()”, and sets it to 0. It has a public access specifier.

--- shooter/arm.java
+++ shooter/arm.java
@@ -1,3 +1,14 @@
 public class arm {
+      /** Creates a new Arm. */
+    public void Arm() {
+        TalonFX arm = new TalonFX(ArmConstants.armLeaderId);
+        arm.configFactoryDefault(); // Call the method to reset configurations to default
+        arm.setNeutralMode(NeutralModeValue.Brake); // Set the neutral mode to Brake
+
+        public void resetArmEncoder() {
+            armLeader.setSelectedSensorPosition(0); // Reset the encoder position to 0
+        }
+        
+    }
     
 }
